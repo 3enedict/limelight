8c29a3e5c3292b1425dbda3e66c28fbd6c34ff05
Benoit Poitevin <benoit.philip.poitevin@gmail.com>
2022-12-17 13:47:08 +0100

    App start

---
+++ lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Limelight());
+}
+
+class Limelight extends StatelessWidget {
+  const Limelight({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Limelight',
+      home: IngredientsPage(),
+    );
+  }
+}
+
+class IngredientsPage extends StatelessWidget {
+  const IngredientsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("Ingredients"),
+        ),
+        bottomNavigationBar:
+            BottomNavigationBar(items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.business),
+            label: 'Business',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.school),
+            label: 'School',
+          ),
+        ]),
+        body: const Center(child: Text("Yo")));
+  }
+}
