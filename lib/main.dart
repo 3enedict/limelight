10983ae8f0adf52dd54d2fe14f143b8ee61d64e3
Benoit Poitevin <benoit.philip.poitevin@gmail.com>
2022-12-20 18:24:59 +0100

    Added IngredientSlider to easily add ingredients.

--- lib/main.dart
+++ lib/main.dart
@@ -48,86 +48,52 @@ class IngredientsPage extends StatelessWidget {
         ),
         floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
         body: Column(
-          children: <Widget>[
-            Center(
-                child: Container(
-                    color: Colors.grey,
-                    padding: const EdgeInsets.all(20.0),
-                    margin: const EdgeInsets.all(20.0),
-                    child: Row(
-                      children: <Widget>[
-                        Flexible(
-                          child: Container(
-                              color: Colors.white,
-                              padding: const EdgeInsets.all(5.0),
-                              margin: const EdgeInsets.all(10.0),
-                              child: Column(children: const <Widget>[
-                                Icon(Icons.image),
-                                Text('Lemon')
-                              ])),
-                        ),
-                        Flexible(
-                          child: Container(
-                              color: Colors.white,
-                              padding: const EdgeInsets.all(5.0),
-                              margin: const EdgeInsets.all(10.0),
-                              child: Column(children: const <Widget>[
-                                Icon(Icons.image),
-                                Text('Orange')
-                              ])),
-                        ),
-                        Flexible(
-                          child: Container(
-                              color: Colors.white,
-                              padding: const EdgeInsets.all(5.0),
-                              margin: const EdgeInsets.all(10.0),
-                              child: Column(children: const <Widget>[
-                                Icon(Icons.image),
-                                Text('Pineapple')
-                              ])),
-                        ),
-                      ],
-                    ))),
-            Center(
-                child: Container(
-                    color: Colors.grey,
-                    padding: const EdgeInsets.all(20.0),
-                    margin: const EdgeInsets.all(20.0),
-                    child: Row(
-                      children: <Widget>[
-                        Flexible(
-                          child: Container(
-                              color: Colors.white,
-                              padding: const EdgeInsets.all(5.0),
-                              margin: const EdgeInsets.all(10.0),
-                              child: Column(children: const <Widget>[
-                                Icon(Icons.image),
-                                Text('Carrot')
-                              ])),
-                        ),
-                        Flexible(
-                          child: Container(
-                              color: Colors.white,
-                              padding: const EdgeInsets.all(5.0),
-                              margin: const EdgeInsets.all(10.0),
-                              child: Column(children: const <Widget>[
-                                Icon(Icons.image),
-                                Text('Tomato')
-                              ])),
-                        ),
-                        Flexible(
-                          child: Container(
-                              color: Colors.white,
-                              padding: const EdgeInsets.all(5.0),
-                              margin: const EdgeInsets.all(10.0),
-                              child: Column(children: const <Widget>[
-                                Icon(Icons.image),
-                                Text('Eggplant')
-                              ])),
-                        ),
-                      ],
-                    ))),
+          children: const <Widget>[
+            IngredientSlider(items: {
+              'banana': Icon(Icons.image),
+              'orange': Icon(Icons.image),
+              'apple': Icon(Icons.image)
+            }),
+            IngredientSlider(items: {
+              'broccoli': Icon(Icons.image),
+              'eggplant': Icon(Icons.image),
+              'carrot': Icon(Icons.image)
+            }),
           ],
         ));
+  }
+}
+
+class IngredientSlider extends StatelessWidget {
+  final Map<String, Icon> items;
+  const IngredientSlider({super.key, required this.items});
+
+  @override
+  Widget build(BuildContext context) {
+    List<Widget> ingredients = [];
+
+    for (final mapEntry in items.entries) {
+      final ingredientName = mapEntry.key;
+      final ingredientIcon = mapEntry.value;
+
+      ingredients.add(Flexible(
+        child: Container(
+            color: Colors.white,
+            padding: const EdgeInsets.all(5.0),
+            margin: const EdgeInsets.all(10.0),
+            child: Column(
+                children: <Widget>[ingredientIcon, Text(ingredientName)])),
+      ));
+    }
+
+    return Center(
+        child: Container(
+      color: Colors.grey,
+      padding: const EdgeInsets.all(20.0),
+      margin: const EdgeInsets.all(20.0),
+      child: Row(
+        children: ingredients,
+      ),
+    ));
   }
 }
