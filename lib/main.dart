fb046794cc829c10b2b1388e9c79d2437f4a5f2f
Benoit Poitevin <benoit.philip.poitevin@gmail.com>
2022-12-22 17:54:43 +0100

    Added cards corresponding to the ingredient's type

--- lib/main.dart
+++ lib/main.dart
@@ -41,41 +41,180 @@ class IngredientsPage extends StatelessWidget {
             "Ingredients",
             textAlign: TextAlign.center,
             style: GoogleFonts.raleway(
-                textStyle: const TextStyle(fontWeight: FontWeight.bold)),
-          ),
-          centerTitle: true,
-          backgroundColor: Colors.transparent,
-          elevation: 0,
-          bottom: PreferredSize(
-            preferredSize: const Size.fromHeight(4.0),
-            child: Container(
-              color: Colors.white38,
-              height: 1.0,
-              margin: const EdgeInsets.fromLTRB(40.0, 0.0, 40.0, 0.0),
+              textStyle: const TextStyle(
+                fontWeight: FontWeight.bold,
+                color: Colors.white70,
+              ),
             ),
           ),
+          leading: const Icon(
+            Icons.menu,
+            color: Colors.white70,
+          ),
+          backgroundColor: Colors.transparent,
+          elevation: 0,
         ),
         floatingActionButton: FloatingActionButton(
           child: const Icon(Icons.add),
           onPressed: () {},
         ),
-        body: Column(
+        body: ListView(
+          padding: const EdgeInsets.all(20),
           children: <Widget>[
             Container(
               decoration: const BoxDecoration(
-                color: Color(0xFF29263a),
+                gradient: LinearGradient(
+                  colors: [
+                    Color(0xFF02AABD),
+                    Color(0xFF00CDAC),
+                  ],
+                ),
                 borderRadius: BorderRadius.all(Radius.circular(10)),
                 boxShadow: [
                   BoxShadow(
-                    color: Colors.black87,
-                    spreadRadius: 2,
-                    blurRadius: 5,
+                    color: Colors.black54,
+                    spreadRadius: 3,
+                    blurRadius: 10,
                   ),
                 ],
               ),
-              padding: const EdgeInsets.all(8.0),
-              margin: const EdgeInsets.all(8.0),
-              child: const Text("Veggies"),
+              alignment: Alignment.center,
+              padding: const EdgeInsets.all(10.0),
+              margin: const EdgeInsets.all(10.0),
+              height: 100,
+              child: Text(
+                'Vegetables',
+                style: GoogleFonts.fredokaOne(
+                  textStyle: const TextStyle(
+                    color: Colors.black87,
+                    fontSize: 20,
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [
+                    Color(0xFF614385),
+                    Color(0xFF516395),
+                  ],
+                ),
+                borderRadius: BorderRadius.all(Radius.circular(10)),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black54,
+                    spreadRadius: 3,
+                    blurRadius: 10,
+                  ),
+                ],
+              ),
+              alignment: Alignment.center,
+              padding: const EdgeInsets.all(10.0),
+              margin: const EdgeInsets.all(10.0),
+              height: 100,
+              child: Text(
+                'Fruits',
+                style: GoogleFonts.fredokaOne(
+                  textStyle: const TextStyle(
+                    color: Colors.black87,
+                    fontSize: 20,
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [
+                    Color(0xFFFF512F),
+                    Color(0xFFDD2476),
+                  ],
+                ),
+                borderRadius: BorderRadius.all(Radius.circular(10)),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black54,
+                    spreadRadius: 3,
+                    blurRadius: 10,
+                  ),
+                ],
+              ),
+              alignment: Alignment.center,
+              padding: const EdgeInsets.all(10.0),
+              margin: const EdgeInsets.all(10.0),
+              height: 100,
+              child: Text(
+                'Meat & Eggs',
+                style: GoogleFonts.fredokaOne(
+                  textStyle: const TextStyle(
+                    color: Colors.black87,
+                    fontSize: 20,
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [
+                    Color(0xFFA9F1DF),
+                    Color(0xFFFFBBBB),
+                  ],
+                ),
+                borderRadius: BorderRadius.all(Radius.circular(10)),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black54,
+                    spreadRadius: 3,
+                    blurRadius: 10,
+                  ),
+                ],
+              ),
+              alignment: Alignment.center,
+              padding: const EdgeInsets.all(10.0),
+              margin: const EdgeInsets.all(10.0),
+              height: 100,
+              child: Text(
+                'Seafood',
+                style: GoogleFonts.fredokaOne(
+                  textStyle: const TextStyle(
+                    color: Colors.black87,
+                    fontSize: 20,
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [
+                    Color(0xFFEE9CA7),
+                    Color(0xFFFFDDE1),
+                  ],
+                ),
+                borderRadius: BorderRadius.all(Radius.circular(10)),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black54,
+                    spreadRadius: 3,
+                    blurRadius: 10,
+                  ),
+                ],
+              ),
+              alignment: Alignment.center,
+              padding: const EdgeInsets.all(10.0),
+              margin: const EdgeInsets.all(10.0),
+              height: 100,
+              child: Text(
+                'Dairy',
+                style: GoogleFonts.fredokaOne(
+                  textStyle: const TextStyle(
+                    color: Colors.black87,
+                    fontSize: 20,
+                  ),
+                ),
+              ),
             ),
           ],
         ),
