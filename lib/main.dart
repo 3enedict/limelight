0ecc8eda78cccae6817eef32c9cb6bde87ef2315
Benoit Poitevin <benoit.philip.poitevin@gmail.com>
2022-12-24 14:23:09 +0100

    Finished example of ingredient arrangement...

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fluttericon/font_awesome5_icons.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main() {
@@ -54,170 +55,139 @@ class IngredientsPage extends StatelessWidget {
           backgroundColor: Colors.transparent,
           elevation: 0,
         ),
-        floatingActionButton: FloatingActionButton(
-          child: const Icon(Icons.add),
-          onPressed: () {},
+        floatingActionButton: Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            FloatingActionButton(
+              onPressed: () {},
+              child: const Icon(Icons.delete),
+            ),
+            const SizedBox(
+              width: 10,
+            ),
+            FloatingActionButton(
+              onPressed: () => {},
+              child: const Icon(Icons.star),
+            )
+          ],
         ),
         body: ListView(
           padding: const EdgeInsets.all(20),
-          children: <Widget>[
-            Container(
-              decoration: const BoxDecoration(
-                gradient: LinearGradient(
-                  colors: [
-                    Color(0xFF02AABD),
-                    Color(0xFF00CDAC),
-                  ],
-                ),
-                borderRadius: BorderRadius.all(Radius.circular(10)),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black54,
-                    spreadRadius: 3,
-                    blurRadius: 10,
-                  ),
-                ],
-              ),
-              alignment: Alignment.center,
-              padding: const EdgeInsets.all(10.0),
-              margin: const EdgeInsets.all(10.0),
-              height: 100,
-              child: Text(
-                'Vegetables',
-                style: GoogleFonts.fredokaOne(
-                  textStyle: const TextStyle(
-                    color: Colors.black87,
-                    fontSize: 20,
-                  ),
-                ),
-              ),
-            ),
-            Container(
-              decoration: const BoxDecoration(
-                gradient: LinearGradient(
-                  colors: [
-                    Color(0xFF614385),
-                    Color(0xFF516395),
-                  ],
-                ),
-                borderRadius: BorderRadius.all(Radius.circular(10)),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black54,
-                    spreadRadius: 3,
-                    blurRadius: 10,
-                  ),
-                ],
-              ),
-              alignment: Alignment.center,
-              padding: const EdgeInsets.all(10.0),
-              margin: const EdgeInsets.all(10.0),
-              height: 100,
-              child: Text(
-                'Fruits',
-                style: GoogleFonts.fredokaOne(
-                  textStyle: const TextStyle(
-                    color: Colors.black87,
-                    fontSize: 20,
-                  ),
-                ),
-              ),
-            ),
-            Container(
-              decoration: const BoxDecoration(
-                gradient: LinearGradient(
-                  colors: [
-                    Color(0xFFFF512F),
-                    Color(0xFFDD2476),
-                  ],
-                ),
-                borderRadius: BorderRadius.all(Radius.circular(10)),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black54,
-                    spreadRadius: 3,
-                    blurRadius: 10,
-                  ),
-                ],
-              ),
-              alignment: Alignment.center,
-              padding: const EdgeInsets.all(10.0),
-              margin: const EdgeInsets.all(10.0),
-              height: 100,
-              child: Text(
-                'Meat & Eggs',
-                style: GoogleFonts.fredokaOne(
-                  textStyle: const TextStyle(
-                    color: Colors.black87,
-                    fontSize: 20,
-                  ),
-                ),
-              ),
-            ),
-            Container(
-              decoration: const BoxDecoration(
-                gradient: LinearGradient(
-                  colors: [
-                    Color(0xFFA9F1DF),
-                    Color(0xFFFFBBBB),
-                  ],
-                ),
-                borderRadius: BorderRadius.all(Radius.circular(10)),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black54,
-                    spreadRadius: 3,
-                    blurRadius: 10,
-                  ),
-                ],
-              ),
-              alignment: Alignment.center,
-              padding: const EdgeInsets.all(10.0),
-              margin: const EdgeInsets.all(10.0),
-              height: 100,
-              child: Text(
-                'Seafood',
-                style: GoogleFonts.fredokaOne(
-                  textStyle: const TextStyle(
-                    color: Colors.black87,
-                    fontSize: 20,
-                  ),
-                ),
-              ),
-            ),
-            Container(
-              decoration: const BoxDecoration(
-                gradient: LinearGradient(
-                  colors: [
-                    Color(0xFFEE9CA7),
-                    Color(0xFFFFDDE1),
-                  ],
-                ),
-                borderRadius: BorderRadius.all(Radius.circular(10)),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black54,
-                    spreadRadius: 3,
-                    blurRadius: 10,
-                  ),
-                ],
-              ),
-              alignment: Alignment.center,
-              padding: const EdgeInsets.all(10.0),
-              margin: const EdgeInsets.all(10.0),
-              height: 100,
-              child: Text(
-                'Dairy',
-                style: GoogleFonts.fredokaOne(
-                  textStyle: const TextStyle(
-                    color: Colors.black87,
-                    fontSize: 20,
-                  ),
-                ),
-              ),
-            ),
+          children: const <Widget>[
+            IngredientsList(),
+            IngredientsList(),
+            IngredientsList(),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class IngredientsList extends StatelessWidget {
+  const IngredientsList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        const Text(
+          'Vegetables',
+          style: TextStyle(
+            color: Colors.white70,
+            fontSize: 15,
+          ),
+        ),
+        Container(
+          decoration: const BoxDecoration(
+            color: Color(0xFF2f2c43),
+            borderRadius: BorderRadius.all(Radius.circular(10)),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black54,
+                spreadRadius: 5,
+                blurRadius: 10,
+              ),
+            ],
+          ),
+          padding: const EdgeInsets.all(10.0),
+          margin: const EdgeInsets.fromLTRB(0, 20, 0, 20),
+          alignment: Alignment.center,
+          child: GridView.builder(
+            shrinkWrap: true,
+            primary: false,
+            padding: const EdgeInsets.all(10),
+            gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+              maxCrossAxisExtent: 100,
+              crossAxisSpacing: 10,
+              mainAxisSpacing: 10,
+              childAspectRatio: 0.9,
+            ),
+            itemCount: 8,
+            itemBuilder: (context, index) {
+              return const GridTile(child: Ingredient());
+            },
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class Ingredient extends StatelessWidget {
+  const Ingredient({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: const Color(0xFF2f2c43),
+        borderRadius: const BorderRadius.all(Radius.circular(10)),
+        border: Border.all(
+          color: Colors.white70,
+          width: 1,
+        ),
+        boxShadow: const [
+          BoxShadow(
+            color: Colors.black54,
+            spreadRadius: 2,
+            blurRadius: 5,
+          ),
+        ],
+      ),
+      padding: const EdgeInsets.fromLTRB(10, 5, 10, 5),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          Expanded(
+            child: FittedBox(
+              child: ShaderMask(
+                blendMode: BlendMode.srcATop,
+                shaderCallback: (bounds) {
+                  return const LinearGradient(
+                    colors: <Color>[
+                      Color(0xFF3fa7cd),
+                      Color(0xFF45cac6),
+                    ],
+                  ).createShader(bounds);
+                },
+                child: const Icon(FontAwesome5.lemon),
+              ),
+            ),
+          ),
+          const Flexible(
+            child: FittedBox(
+              child: Text(
+                'Lime',
+                style: TextStyle(
+                  color: Colors.white70,
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
