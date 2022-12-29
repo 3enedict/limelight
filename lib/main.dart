6bb57af05856c9ca02c55ab8b5cc0a881700841f
Benoit Poitevin <benoit.philip.poitevin@gmail.com>
2022-12-29 17:52:04 +0100

    Went back to basics with simple scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:fluttericon/font_awesome5_icons.dart';
+import 'package:fluttericon/rpg_awesome_icons.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main() {
+  Paint.enableDithering = true;
   runApp(const Limelight());
 }
 
@@ -25,11 +27,11 @@ class IngredientsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      decoration: const BoxDecoration(
+      decoration: BoxDecoration(
         gradient: LinearGradient(
           colors: [
-            Color(0xFF2e2b44),
-            Color(0xFF221f31),
+            Colors.black87,
+            Colors.grey[900]!,
           ],
           begin: Alignment.topCenter,
           end: Alignment.bottomCenter,
@@ -40,121 +42,48 @@ class IngredientsPage extends StatelessWidget {
         appBar: AppBar(
           title: Text(
             "Ingredients",
-            textAlign: TextAlign.center,
-            style: GoogleFonts.raleway(
-              textStyle: const TextStyle(
+            style: GoogleFonts.openSans(
+              textStyle: TextStyle(
                 fontWeight: FontWeight.bold,
-                color: Colors.white70,
+                color: Colors.grey[50],
               ),
             ),
           ),
-          leading: const Icon(
-            Icons.menu,
-            color: Colors.white70,
-          ),
+          centerTitle: true,
           backgroundColor: Colors.transparent,
           elevation: 0,
         ),
-        floatingActionButton: Row(
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: [
-            FloatingActionButton(
-              onPressed: () {},
-              child: const Icon(Icons.delete),
+        bottomNavigationBar: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          selectedItemColor: Colors.white70,
+          unselectedItemColor: Colors.white30,
+          showSelectedLabels: false,
+          showUnselectedLabels: false,
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(FontAwesome5.leaf),
+              label: 'Leafy greens',
             ),
-            const SizedBox(
-              width: 10,
+            BottomNavigationBarItem(
+              icon: Icon(FontAwesome5.carrot),
+              label: 'Vegetables',
             ),
-            FloatingActionButton(
-              onPressed: () => {},
-              child: const Icon(Icons.star),
-            )
+            BottomNavigationBarItem(
+              icon: Icon(RpgAwesome.meat),
+              label: 'Meat & Eggs',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(FontAwesome5.fish),
+              label: 'Fish',
+            ),
           ],
         ),
-        body: ListView(
-          padding: const EdgeInsets.all(20),
-          children: const <Widget>[
-            IngredientsList(),
-            IngredientsList(),
-            IngredientsList(),
-          ],
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: const Icon(Icons.search),
         ),
-      ),
-    );
-  }
-}
-
-class IngredientsList extends StatelessWidget {
-  const IngredientsList({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      mainAxisSize: MainAxisSize.min,
-      children: <Widget>[
-        const Text(
-          'Vegetables',
-          style: TextStyle(
-            color: Colors.white70,
-            fontSize: 15,
-          ),
-        ),
-        Container(
-          decoration: const BoxDecoration(
-            color: Color(0xFF2f2c43),
-            borderRadius: BorderRadius.all(Radius.circular(10)),
-          ),
-          padding: const EdgeInsets.all(10.0),
-          margin: const EdgeInsets.fromLTRB(0, 20, 0, 20),
-          alignment: Alignment.center,
-          child: GridView(
-            shrinkWrap: true,
-            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2,
-              childAspectRatio: 3,
-            ),
-            children: const [
-              Ingredient(text: 'Lemon', icon: Icon(FontAwesome5.lemon)),
-              Ingredient(text: 'Lemon', icon: Icon(FontAwesome5.lemon)),
-              Ingredient(text: 'Lemon', icon: Icon(FontAwesome5.lemon)),
-              Ingredient(text: 'Lemon', icon: Icon(FontAwesome5.lemon)),
-            ],
-          ),
-        ),
-      ],
-    );
-  }
-}
-
-class Ingredient extends StatelessWidget {
-  final String text;
-  final Icon icon;
-
-  const Ingredient({super.key, required this.text, required this.icon});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      decoration: const BoxDecoration(
-        color: Color(0xFF3a374d),
-        borderRadius: BorderRadius.all(Radius.circular(5)),
-      ),
-      padding: const EdgeInsets.all(10),
-      margin: const EdgeInsets.all(10),
-      child: Row(
-        children: [
-          icon,
-          const SizedBox(width: 10),
-          Expanded(
-            child: Text(
-              text,
-              style: const TextStyle(
-                color: Colors.white70,
-              ),
-            ),
-          ),
-        ],
       ),
     );
   }
