691ac8ed74e5a48ca71582c68d26cd68c65bdedc
Benoit Poitevin <benoit.philip.poitevin@gmail.com>
2023-02-11 10:11:05 +0100

    Added gradient to smooth out bottom app bar

--- lib/main.dart
+++ lib/main.dart
@@ -101,77 +101,97 @@ class LeafyGreensPage extends StatelessWidget {
         onPressed: () {},
         child: const Icon(Icons.search),
       ),
-      body: CustomScrollView(
-        slivers: <Widget>[
-          SliverAppBar(
-            pinned: true,
-            snap: false,
-            floating: false,
-            expandedHeight: 200.0,
-            backgroundColor: const Color(0xFF222222),
-            flexibleSpace: FlexibleSpaceBar(
-              centerTitle: true,
-              title: Text(
-                'Leafy greens',
-                style: GoogleFonts.workSans(
-                  textStyle: const TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontStyle: FontStyle.italic,
-                    color: Colors.white,
-                  ),
-                ),
-              ),
-              background: Container(
-                decoration: const BoxDecoration(
-                  borderRadius: BorderRadius.vertical(
-                    top: Radius.circular(0),
-                    bottom: Radius.circular(20),
-                  ),
-                  image: DecorationImage(
-                    alignment: Alignment.topCenter,
-                    fit: BoxFit.cover,
-                    colorFilter: ColorFilter.mode(
-                      Colors.grey,
-                      BlendMode.saturation,
-                    ),
-                    image: AssetImage('assets/Leafy Greeens.jpg'),
-                  ),
-                ),
-                child: Container(
-                  color: Colors.black38,
-                ),
-              ),
-            ),
-          ),
-          SliverList(
-            delegate: SliverChildListDelegate(
-              leafyGreens
-                  .map(
-                    (name) => Container(
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(12),
-                        gradient: const LinearGradient(
-                          colors: [
-                            Color(0xFF555555),
-                            Color(0xFF222222),
-                          ],
-                          begin: Alignment.centerLeft,
-                          end: Alignment.centerRight,
-                        ),
+      body: Stack(
+        children: [
+          CustomScrollView(
+            slivers: <Widget>[
+              SliverAppBar(
+                pinned: true,
+                snap: false,
+                floating: false,
+                expandedHeight: 200.0,
+                backgroundColor: const Color(0xFF222222),
+                flexibleSpace: FlexibleSpaceBar(
+                  centerTitle: true,
+                  title: Text(
+                    'Leafy greens',
+                    style: GoogleFonts.workSans(
+                      textStyle: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontStyle: FontStyle.italic,
+                        color: Colors.white,
                       ),
-                      margin: const EdgeInsets.fromLTRB(20, 15, 20, 0),
-                      padding: const EdgeInsets.all(25),
-                      child: Text(
-                        name,
-                        style: GoogleFonts.workSans(
-                          textStyle: const TextStyle(
-                            color: Colors.white,
+                    ),
+                  ),
+                  background: Container(
+                    decoration: const BoxDecoration(
+                      borderRadius: BorderRadius.vertical(
+                        top: Radius.circular(0),
+                        bottom: Radius.circular(20),
+                      ),
+                      image: DecorationImage(
+                        alignment: Alignment.topCenter,
+                        fit: BoxFit.cover,
+                        colorFilter: ColorFilter.mode(
+                          Colors.grey,
+                          BlendMode.saturation,
+                        ),
+                        image: AssetImage('assets/Leafy Greeens.jpg'),
+                      ),
+                    ),
+                    child: Container(
+                      color: Colors.black38,
+                    ),
+                  ),
+                ),
+              ),
+              SliverList(
+                delegate: SliverChildListDelegate(
+                  leafyGreens
+                      .map(
+                        (name) => Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(12),
+                            gradient: const LinearGradient(
+                              colors: [
+                                Color(0xFF444444),
+                                Color(0xFF333333),
+                              ],
+                              begin: Alignment.centerLeft,
+                              end: Alignment.centerRight,
+                            ),
+                          ),
+                          margin: const EdgeInsets.fromLTRB(20, 15, 20, 0),
+                          padding: const EdgeInsets.all(25),
+                          child: Text(
+                            name,
+                            style: GoogleFonts.workSans(
+                              textStyle: const TextStyle(
+                                color: Colors.white,
+                              ),
+                            ),
                           ),
                         ),
-                      ),
-                    ),
-                  )
-                  .toList(),
+                      )
+                      .toList(),
+                ),
+              ),
+            ],
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [
+                    Color(0xFF111111),
+                    Color(0x00111111),
+                  ],
+                  begin: Alignment.bottomCenter,
+                  end: Alignment.topCenter,
+                ),
+              ),
+              height: 150,
             ),
           ),
         ],
