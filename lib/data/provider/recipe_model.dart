4f6765d70ca253f703ceeb6ad2446afd9a6190d9
Benoit Poitevin <benoit.philip.poitevin@gmail.com>
2023-07-26 09:48:06 +0200

    RecipeModel can now generate an instruction set.

--- lib/data/provider/recipe_model.dart
+++ lib/data/provider/recipe_model.dart
@@ -97,4 +97,138 @@ class RecipeModel extends ChangeNotifier {
 
     return ingredients;
   }
+
+  List<String> instructionSet(
+    int recipeId,
+    int numberOfServings,
+    List<(int, int)> variationIds,
+  ) {
+    String instructions = _recipe(recipeId).instructions.join("(Enter)");
+
+    instructions =
+        _replaceVariationInstructions(recipeId, instructions, variationIds);
+    instructions =
+        _replaceGlobalIngredients(recipeId, numberOfServings, instructions);
+    instructions = _replaceVariationIngredients(
+        recipeId, numberOfServings, instructions, variationIds);
+
+    return instructions.split("(Enter)");
+  }
+
+  String _replaceVariationInstructions(
+    int recipeId,
+    String instructions,
+    List<(int, int)> variationIds,
+  ) {
+    String localInstructions = instructions;
+    final variationInstructionsRegex = RegExp(
+      r'\{([0-9]+):([0-9]+):([0-9]+):instruction\}',
+    );
+
+    for (var match in variationInstructionsRegex.allMatches(instructions)) {
+      final variationGroupId = int.parse(match.group(1) ?? "-1");
+      final variationId = int.parse(match.group(2) ?? "-1");
+      final instructionId = int.parse(match.group(3) ?? "-1");
+
+      final old = "{$variationGroupId:$variationId:$instructionId:instruction}";
+
+      if (variationIds.contains((variationGroupId, variationId))) {
+        final newInstructions =
+            _variation(recipeId, variationGroupId, variationId)
+                .instructionGroups[instructionId]
+                .join("(Enter)");
+
+        localInstructions = localInstructions.replaceAll(old, newInstructions);
+      } else {
+        localInstructions = localInstructions.replaceAll("$old(Enter)", "");
+      }
+    }
+
+    return localInstructions;
+  }
+
+  String _replaceGlobalIngredients(
+    int recipeId,
+    int numberOfServings,
+    String instructions,
+  ) {
+    String localInstructions = instructions;
+    final ingredientsRegex = RegExp(
+      r'\{([0-9]+):quantity\}',
+    );
+
+    for (var match in ingredientsRegex.allMatches(instructions)) {
+      final instructionId = int.parse(match.group(1) ?? "-1");
+
+      localInstructions = localInstructions.replaceAll(
+        "{$instructionId:quantity}",
+        _computeIngredientQuantities(
+          numberOfServings,
+          _recipe(recipeId).ingredients[instructionId],
+        ),
+      );
+    }
+
+    return localInstructions;
+  }
+
+  String _replaceVariationIngredients(
+    int recipeId,
+    int numberOfServings,
+    String instructions,
+    List<(int, int)> variationIds,
+  ) {
+    String localInstructions = instructions;
+    final variationIngredientsRegex = RegExp(
+      r'\{([0-9]+):([0-9]+):([0-9]+):quantity\}',
+    );
+
+    for (var match in variationIngredientsRegex.allMatches(instructions)) {
+      final variationGroupId = int.parse(match.group(1) ?? "-1");
+      final variationId = int.parse(match.group(2) ?? "-1");
+      final instructionId = int.parse(match.group(3) ?? "-1");
+
+      localInstructions = localInstructions.replaceAll(
+        "{$variationGroupId:$variationId:$instructionId:quantity}",
+        _computeIngredientQuantities(
+          numberOfServings,
+          _variation(recipeId, variationGroupId, variationId)
+              .ingredients[instructionId],
+        ),
+      );
+    }
+
+    return localInstructions;
+  }
+
+  String _computeIngredientQuantities(
+    int numberOfServings,
+    IngredientData ingredient,
+  ) {
+    double? quantity = double.tryParse(ingredient.quantity);
+
+    if (quantity == null) {
+      String quantity = ingredient.quantity.replaceAll(RegExp(r'[^0-9]'), '');
+      if (quantity == "") {
+        return "${ingredient.quantity.toLowerCase()} of ${ingredient.name.toLowerCase()}";
+      }
+
+      int number = (numberOfServings * double.parse(quantity)).floor();
+      if (number == 0) number = 1;
+
+      String unit = ingredient.quantity.replaceAll(quantity, "");
+      String name = ingredient.name;
+
+      return "$number$unit of $name".toLowerCase();
+    } else {
+      int number = (numberOfServings * quantity).floor();
+      if (number == 0) number = 1;
+
+      String ingredientName = number == 1
+          ? ingredient.name.replaceAll(RegExp(r'\(.*\)'), '')
+          : ingredient.name.replaceAll('(', '').replaceAll(')', '');
+
+      return "$number $ingredientName".toLowerCase();
+    }
+  }
 }
--- test/recipe_model_test.dart
+++ test/recipe_model_test.dart
@@ -13,7 +13,7 @@ final simpleRecipe = RecipeData(
   ingredients: [
     IngredientData(
       name: "Egg(s)",
-      quantity: "1",
+      quantity: "0.8",
     ),
     IngredientData(
       name: "Flour",
@@ -29,13 +29,13 @@ final simpleRecipe = RecipeData(
     ),
   ],
   instructions: [
-    "Knead {Flour:quantity}, some salt and {Egg(s):quantity} into a smooth dough that springs back to the touch.",
+    "Knead {1:quantity}, some salt and {0:quantity} into a smooth dough that springs back to the touch.",
     "After waiting for at least 30 minutes, roll out the dough.",
     "Set the rolled out dough to dry until parchment-like. This should take about an hour or two.",
     "After having rolled up the dough, cut it into small strips.",
-    "Finely grate {Parmesan:quantity}.",
+    "Finely grate {2:quantity}.",
     "Cook the pasta in a small amount of boiling water.",
-    "Put {Butter:quantity} in a cold pan and stir your cooked pasta, some of the water, and your parmesan.",
+    "Put {3:quantity} in a cold pan and stir your cooked pasta, some of the water, and your parmesan.",
   ],
   variationGroups: [],
 );
@@ -45,81 +45,111 @@ final complicatedRecipe = RecipeData(
   difficulty: "Easy",
   price: "\$1",
   ingredients: [
-    IngredientData(name: "Olive oil", quantity: "2tbsp of olive oil"),
+    IngredientData(name: "Olive oil", quantity: "2tbsp"),
   ],
   instructions: [
-    "{0:0:0}",
-    "{1:0:0}",
-    "{1:1:0}",
-    "{2:0:0}",
-    "{2:1:0}",
+    "{0:0:0:instruction}",
+    "{1:0:0:instruction}",
+    "{1:1:0:instruction}",
+    "{2:0:0:instruction}",
+    "{2:1:0:instruction}",
     "Salt to taste",
-    "{0:0:1}",
-    "{0:1:0}",
+    "{0:0:1:instruction}",
+    "{0:1:0:instruction}",
     "Drain the pasta, while keeping some of the water that it cooked in.",
     "Mix the drained pasta into the sauce, adding some of the pasta water until everything has a good consistency."
   ],
   variationGroups: [
-    VariationGroup(groupName: "Type of pasta", variations: [
-      Variation(name: "Tagliatelle", time: "+1 hour", ingredients: [
-        IngredientData(
-          name: "Flour",
-          quantity: "100g of flour",
+    VariationGroup(
+      groupName: "Type of pasta",
+      variations: [
+        Variation(
+          name: "Tagliatelle",
+          time: "+1 hour",
+          ingredients: [
+            IngredientData(name: "Flour", quantity: "100g"),
+            IngredientData(name: "Egg(s)", quantity: "1"),
+          ],
+          instructionGroups: [
+            [
+              "Knead {0:0:0:quantity}, some salt and {0:0:1:quantity} into a smooth dough that springs back to the touch.",
+              "After waiting for at least 30 minutes, roll out the dough.",
+              "Set the rolled out dough to dry until parchment-like. This should take about an hour or two.",
+              "After having rolled up the dough, cut it into small strips."
+            ],
+            [
+              "Pour all of your pasta in the boiling water",
+            ],
+          ],
         ),
-        IngredientData(
-          name: "Egg(s)",
-          quantity: "1",
+        Variation(
+          name: "Dry pasta",
+          time: "+10 min",
+          ingredients: [
+            IngredientData(name: "Dry pasta", quantity: ""),
+          ],
+          instructionGroups: [
+            [
+              "Pour your pasta, following the instructions on the packet, in the boiling water."
+            ]
+          ],
         ),
-      ], instructionGroups: [
-        [
-          "Knead {Flour:quantity}, some salt and {Egg(s):quantity} into a smooth dough that springs back to the touch.",
-          "After waiting for at least 30 minutes, roll out the dough.",
-          "Set the rolled out dough to dry until parchment-like. This should take about an hour or two.",
-          "After having rolled up the dough, cut it into small strips."
-        ],
-        ["Pour all of your pasta in the boiling water"]
-      ]),
-      Variation(name: "Dry pasta", time: "+10 min", ingredients: [
-        IngredientData(name: "Dry pasta", quantity: ""),
-      ], instructionGroups: [
-        [
-          "Pour your pasta, following the instructions on the packet, in the boiling water."
-        ]
-      ]),
-    ]),
-    VariationGroup(groupName: "Type of base for the sauce", variations: [
-      Variation(name: "Soffritto", time: "+10 min", ingredients: [
-        IngredientData(name: "Onion(s)", quantity: "0.75"),
-        IngredientData(name: "Carrot(s)", quantity: "2"),
-        IngredientData(name: "Celery stick(s)", quantity: "2"),
-      ], instructionGroups: [
-        [
-          "Mince {Onion(s):quantity}, {Carrot(s):quantity} and {Celery stick(s):quantity}.",
-          "Assemble everything in a hot pan with {Olive oil:quantity} and cook until golden brown."
-        ]
-      ]),
-      Variation(name: "Onions and garlic", time: "+10 min", ingredients: [
-        IngredientData(name: "Onion(s)", quantity: "1"),
-        IngredientData(name: "Garlic clove(s)", quantity: "1"),
-      ], instructionGroups: [
-        [
-          "Mince {Onion(s):quantity} and {Garlic clove(s):quantity}.",
-          "Cook the onions in a hot pan with {Olive oil:quantity} until golden brown.",
-          "Add in the garlic and stir for about 30 seconds."
-        ]
-      ]),
-    ]),
+      ],
+    ),
+    VariationGroup(
+      groupName: "Type of base for the sauce",
+      variations: [
+        Variation(
+          name: "Soffritto",
+          time: "+10 min",
+          ingredients: [
+            IngredientData(name: "Onion(s)", quantity: "0.75"),
+            IngredientData(name: "Carrot(s)", quantity: "2"),
+            IngredientData(name: "Celery stick(s)", quantity: "2"),
+          ],
+          instructionGroups: [
+            [
+              "Mince {1:0:0:quantity}, {1:0:1:quantity} and {1:0:2:quantity}.",
+              "Assemble everything in a hot pan with {0:quantity} and cook until golden brown."
+            ]
+          ],
+        ),
+        Variation(
+          name: "Onions and garlic",
+          time: "+10 min",
+          ingredients: [
+            IngredientData(name: "Onion(s)", quantity: "1"),
+            IngredientData(name: "Garlic clove(s)", quantity: "1"),
+          ],
+          instructionGroups: [
+            [
+              "Mince {1:1:0:quantity} and {1:1:1:quantity}.",
+              "Cook the onions in a hot pan with {Olive oil:quantity} until golden brown.",
+              "Add in the garlic and stir for about 30 seconds."
+            ]
+          ],
+        ),
+      ],
+    ),
     VariationGroup(
       groupName: "Type of tomato sauce",
       variations: [
-        Variation(name: "Crushed baby tomatoes", time: "+20 min", ingredients: [
-          IngredientData(name: "Baby tomato(es)", quantity: "40"),
-        ], instructionGroups: [
-          [
-            "Add in about {Baby tomato(es):quantity} to the pan and cover until the skin starts to split.",
-            "Either mash up the tomatoes or wait for them to cook down to a saucy consistency."
-          ]
-        ]),
+        Variation(
+          name: "Crushed baby tomatoes",
+          time: "+20 min",
+          ingredients: [
+            IngredientData(
+              name: "Baby tomato(es)",
+              quantity: "15",
+            ),
+          ],
+          instructionGroups: [
+            [
+              "Add in about {2:0:0:quantity} to the pan and cover until the skin starts to split.",
+              "Either mash up the tomatoes or wait for them to cook down to a saucy consistency."
+            ]
+          ],
+        ),
         Variation(
           name: "Canned tomato sauce",
           time: "+15 min",
@@ -127,9 +157,7 @@ final complicatedRecipe = RecipeData(
             IngredientData(name: "Can(s) of tomato sauce", quantity: "0.75"),
           ],
           instructionGroups: [
-            [
-              "Add in {Can(s) of tomato sauce:quantity} and cook until thickened"
-            ]
+            ["Add in {2:1:0:quantity} and cook until thickened"]
           ],
         ),
       ],
@@ -215,16 +243,68 @@ void main() {
       model.add(complicatedRecipe);
 
       final List<IngredientData> ingredients = [
-        IngredientData(name: "Olive oil", quantity: "2tbsp of olive oil"),
-        IngredientData(name: "Flour", quantity: "100g of flour"),
+        IngredientData(name: "Olive oil", quantity: "2tbsp"),
+        IngredientData(name: "Flour", quantity: "100g"),
         IngredientData(name: "Egg(s)", quantity: "1"),
         IngredientData(name: "Onion(s)", quantity: "1"),
         IngredientData(name: "Garlic clove(s)", quantity: "1"),
-        IngredientData(name: "Baby tomato(es)", quantity: "40"),
+        IngredientData(name: "Baby tomato(es)", quantity: "15"),
       ];
 
       final variations = [(0, 0), (1, 1), (2, 0)];
       expect(model.ingredientList(0, variations), ingredients);
+    });
+
+    test(
+        'Pasta with tomato sauce : generating a list of instructions for a recipe according to it\'s variations produces the correct instructions',
+        () {
+      final RecipeModel model = RecipeModel();
+      model.add(complicatedRecipe);
+
+      final instructions = [
+        "Knead 300g of flour, some salt and 3 eggs into a smooth dough that springs back to the touch.",
+        "After waiting for at least 30 minutes, roll out the dough.",
+        "Set the rolled out dough to dry until parchment-like. This should take about an hour or two.",
+        "After having rolled up the dough, cut it into small strips.",
+        "Mince 2 onions, 6 carrots and 6 celery sticks.",
+        "Assemble everything in a hot pan with 6tbsp of olive oil and cook until golden brown.",
+        "Add in about 45 baby tomatoes to the pan and cover until the skin starts to split.",
+        "Either mash up the tomatoes or wait for them to cook down to a saucy consistency.",
+        "Salt to taste",
+        "Pour all of your pasta in the boiling water",
+        "Drain the pasta, while keeping some of the water that it cooked in.",
+        "Mix the drained pasta into the sauce, adding some of the pasta water until everything has a good consistency.",
+      ];
+
+      const numberOfServings = 3;
+      final variations = [(0, 0), (1, 0), (2, 0)];
+      expect(
+        model.instructionSet(0, numberOfServings, variations),
+        instructions,
+      );
+    });
+
+    test(
+        'Fettucine Alfredo : generating a list of instructions for a recipe according to it\'s variations produces the correct instructions',
+        () {
+      final RecipeModel model = RecipeModel();
+      model.add(simpleRecipe);
+
+      final instructions = [
+        "Knead 100g of flour, some salt and 1 egg into a smooth dough that springs back to the touch.",
+        "After waiting for at least 30 minutes, roll out the dough.",
+        "Set the rolled out dough to dry until parchment-like. This should take about an hour or two.",
+        "After having rolled up the dough, cut it into small strips.",
+        "Finely grate a good handful of parmesan.",
+        "Cook the pasta in a small amount of boiling water.",
+        "Put a good knob of butter in a cold pan and stir your cooked pasta, some of the water, and your parmesan.",
+      ];
+
+      const numberOfServings = 1;
+      expect(
+        model.instructionSet(0, numberOfServings, []),
+        instructions,
+      );
     });
   });
 }
