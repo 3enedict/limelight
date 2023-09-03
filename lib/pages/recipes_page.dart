8fc7b25570d0b075b49d915e4b37042ba25871e1
Benoit Poitevin <benoit.philip.poitevin@gmail.com>
2023-09-03 11:22:47 +0200

    Made recipe buttons not scroll with pageview

--- lib/pages/recipes_page.dart
+++ lib/pages/recipes_page.dart
@@ -20,97 +20,112 @@ class RecipesPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Consumer<RecipeModel>(
       builder: (context, recipes, child) {
-        return PageView(
-          children: List.generate(
-            recipes.number,
-            (int index) {
-              return EmptyPage(
-                appBar: GradientAppBar(
-                  text: CustomText(
-                    text: recipes.name(index),
-                    alignement: TextAlign.center,
-                    size: 20,
-                    weight: FontWeight.w700,
-                  ),
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.fromLTRB(20, 14, 20, 20),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      ConstrainedBox(
-                        constraints: BoxConstraints(
-                          maxHeight: MediaQuery.of(context).size.height / 2.5,
-                        ),
-                        child: RecipeDescriptionBox(
-                          label: "Ingredients",
-                          items: generateIngredients(index, recipes),
+        return Column(
+          children: [
+            Expanded(
+              child: PageView(
+                children: List.generate(
+                  recipes.number,
+                  (int index) {
+                    return EmptyPage(
+                      appBar: GradientAppBar(
+                        text: CustomText(
+                          text: recipes.name(index),
+                          alignement: TextAlign.center,
+                          size: 20,
+                          weight: FontWeight.w700,
                         ),
                       ),
-                      const SizedBox(height: 14),
-                      Expanded(
-                        child: RecipeDescriptionBox(
-                          label: "Instructions",
-                          items: generateInstructions(index, recipes),
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.fromLTRB(30, 25, 30, 5),
-                        child: generateActionButtons(),
-                      )
-                    ],
-                  ),
+                      child: generateContent(context, index, recipes),
+                    );
+                  },
                 ),
-              );
-            },
-          ),
+              ),
+            ),
+            generateActionButtons(),
+          ],
         );
       },
     );
   }
 
+  Widget generateContent(
+      BuildContext context, int recipeId, RecipeModel recipes) {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(20, 14, 20, 20),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          ConstrainedBox(
+            constraints: BoxConstraints(
+              maxHeight: MediaQuery.of(context).size.height / 2.5,
+            ),
+            child: RecipeDescriptionBox(
+              label: "Ingredients",
+              items: generateIngredients(recipeId, recipes),
+            ),
+          ),
+          const SizedBox(height: 14),
+          Expanded(
+            child: RecipeDescriptionBox(
+              label: "Instructions",
+              items: generateInstructions(recipeId, recipes),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
   Widget generateActionButtons() {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        GradientButton(
-          diameter: 58,
-          gradient: toLighterSurfaceGradient(limelightGradient),
-          onPressed: () {},
-          child: Center(
-            child: GradientIcon(
-              gradient: toTextGradient(limelightGradient),
-              icon: Icons.layers,
+    return Container(
+      color: toBackgroundGradient(limelightGradient)[1],
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(30, 0, 30, 20),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            GradientButton(
+              diameter: 52,
+              gradient: toLighterSurfaceGradient(limelightGradient),
+              onPressed: () {},
+              child: Center(
+                child: GradientIcon(
+                  gradient: toTextGradient(limelightGradient),
+                  icon: Icons.layers,
+                ),
+              ),
             ),
-          ),
-        ),
-        const SizedBox(width: 57 / 2),
-        GradientButton(
-          diameter: 58,
-          gradient: toLighterSurfaceGradient(limelightGradient),
-          onPressed: () {},
-          child: const Center(
-            child: GradientIcon(
-              gradient: limelightGradient,
-              icon: UniconsLine.clipboard_notes,
-              size: 27,
+            const SizedBox(width: 51 / 2),
+            GradientButton(
+              diameter: 52,
+              gradient: toLighterSurfaceGradient(limelightGradient),
+              onPressed: () {},
+              child: const Center(
+                child: GradientIcon(
+                  gradient: limelightGradient,
+                  icon: UniconsLine.clipboard_notes,
+                  size: 27,
+                ),
+              ),
             ),
-          ),
-        ),
-        const SizedBox(width: 57 / 2),
-        GradientButton(
-          diameter: 56,
-          gradient: limelightGradient.map((e) => e.withOpacity(0.8)).toList(),
-          onPressed: () {},
-          child: Center(
-            child: GradientIcon(
-              gradient: toSurfaceGradient(limelightGradient),
-              icon: UniconsLine.calender,
-              size: 26,
+            const SizedBox(width: 51 / 2),
+            GradientButton(
+              diameter: 50,
+              gradient:
+                  limelightGradient.map((e) => e.withOpacity(0.8)).toList(),
+              onPressed: () {},
+              child: Center(
+                child: GradientIcon(
+                  gradient: toSurfaceGradient(limelightGradient),
+                  icon: UniconsLine.calender,
+                  size: 26,
+                ),
+              ),
             ),
-          ),
+          ],
         ),
-      ],
+      ),
     );
   }
 }
